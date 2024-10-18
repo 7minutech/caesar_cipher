37ae8b7a38f364fbcdb3a68842612186427edac1
7minutech <elijohnson1107@gmail.com>
2024-10-17 22:50:12 -0500

    feat: added test for get shift num method

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -4,7 +4,7 @@ describe "#shift_char" do
     expect(shift_char("a", 2)).to eql("c")
   end
   it "returns wrapped around char" do
-    expect(shift_char("z", 2)).to eql("a")
+    expect(shift_char("z", 2)).to eql("b")
   end
 end
 
@@ -12,5 +12,11 @@ describe "#not_alpha" do
   it "returns true if not a alpha" do
     expect(not_alpha?("!")).to be true
     expect(not_alpha?("a")).to be false
+  end
+end
+
+describe "#get_shift_num" do
+  it "returns shifted char number" do
+    expect(get_shift_num("a", 3)).to eql(100)
   end
 end
