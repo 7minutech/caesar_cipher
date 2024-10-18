c7f9f2e5296906751d59a8d646bacdfa53816031
7minutech <elijohnson1107@gmail.com>
2024-10-17 22:43:20 -0500

    feat: added test for not alpha method

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -1,7 +1,13 @@
-require_relative "lib/caesar"
-p shift_char("a", 2)
+require "./lib/caesar"
 describe "shift_char" do
   it "returns shifted char" do
     expect(shift_char("a", 2)).to eql("c")
+  end
+end
+
+describe "not_alpha" do
+  it "returns true if not a alpha" do
+    expect(not_alpha?("!")).to be true
+    expect(not_alpha?("a")).to be false
   end
 end
