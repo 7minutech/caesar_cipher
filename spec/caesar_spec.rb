493866f857e44ee0d3b4b9e9b8e32b2a2cce96b9
7minutech <elijohnson1107@gmail.com>
2024-10-17 22:59:42 -0500

    feat: added tests for char_is_valid? method

--- spec/caesar_spec.rb
+++ spec/caesar_spec.rb
@@ -20,3 +20,16 @@ describe "#get_shift_num" do
     expect(get_shift_num("a", 3)).to eql(100)
   end
 end
+
+describe "#char_is_valid?" do
+  it "returns false if char number is over z" do
+    expect(char_is_valid?(122, 3)).to be false
+  end
+  it "return false if char number is over Z" do
+    expect(char_is_valid?(90, 3)).to be false
+  end
+  it "returns true if char number is between a-z and A-Z" do
+    expect(char_is_valid?(105, 1)).to be true
+    expect(char_is_valid?(65, 5)).to be true
+  end
+end
